ba5c993dcfd59fd13b19ab0cb7e894e7abc9eb5a
vitali007tut <vitali007@tut.by>
2023-05-07 20:06:41 +0300

    feat: task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const wayCopy = path.join(__dirname, 'files-copy');
+const wayTarget = path.join(__dirname, 'files');
+
+function makeDir() {
+  fs.mkdir(wayCopy, { recursive: true }, err => {
+    if (err) throw err;
+    // console.log('Папка "files-copy" была создана');
+    readWriteFile()
+  });
+}
+function reCreateDir() {
+  fs.rm(wayCopy, { recursive: true }, err => {
+    if (err) throw err;
+    // console.log('Папка "files-copy" была delete');
+    makeDir()
+    readWriteFile()
+  });
+}
+function readWriteFile() {
+  fs.readdir(wayTarget, (err, files) => {
+    if(err) throw err;
+    files.forEach(e => {
+      // читаю файл
+      fs.readFile(path.join(wayTarget, e), 'utf-8', (err, dataR) => {
+        if (err) throw err;
+        // создаю и заполняю файл
+        fs.writeFile(path.join(wayCopy, e), dataR, (err) => {
+          if (err) throw err;
+      })
+    })
+    })
+  });
+}
+
+fs.access(wayCopy, error => {
+  if (error) { makeDir() }
+  else { reCreateDir() }
+});
 
