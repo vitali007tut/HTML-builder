3465f834c0a8359ab321414cb02a7aa4e0009444
vitali007tut <vitali007@tut.by>
2023-05-07 11:31:47 +0300

    feat: task 3

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -11,28 +11,19 @@ console.log(path.join(__dirname, 'text.txt'))
 //         console.log('Файл был изменен');
 //     }
 // );
-
-// fs.appendFile(path.join(__dirname, 'text.txt'), 'text', (error) => {
-//   if (error) return console.error(error.message);
-//   console.log('Файл создан');
-// });
-
 const { stdin, stdout } = process;
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
-// stdin.pipe(output)
 
 stdout.write('Введи текст, который запишется в файл\n');
 
 stdin.on('data', data => {
-  // stdout.write('Привет, ');
-  // stdin.pipe(data)
-  
+
   if (data.includes('exit')) {
-    process.exit();
+    process.exit(); // выход по содержанию "exit"
   } else {
     output.write(data)
   }
 });
 
-process.on('SIGINT', () => {process.exit();});
+process.on('SIGINT', () => {process.exit();}); // выход по ctrl+c
 process.on('exit', () => stdout.write('Удачи - Good Luck!'));
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const way = path.join(__dirname, 'secret-folder')
+
+fs.promises.readdir(way, { withFileTypes: true })
+    .then(items => {
+        for (let dirent of items) {
+          if (dirent.isFile()) {
+            const element = path.parse(dirent.name)
+            const wayToElement = path.join(way, element.base)
+            fs.stat(wayToElement, (err, stats) => {
+              if (err) {
+                console.log(err);
+              }
+              else {
+                console.log(element.name, element.ext.slice(1), stats.size / 1000 + 'kb')
+              }
+            })
+          }
+        }
+    })
