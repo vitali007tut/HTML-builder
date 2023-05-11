ad3e9d2492e5f86e64178868c9045959882a1635
vitali007tut <vitali007@tut.by>
2023-05-11 16:54:07 +0300

    refactor: task 5 modenate

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,27 +3,36 @@ const path = require('path');
 
 const wayStyles = path.join(__dirname, 'styles');
 const wayProject = path.join(__dirname, 'project-dist');
-const wayBundleFile = path.join(wayProject, 'bundle.css')
-fs.access(wayBundleFile, error => {
-  if (error) {
-    fs.appendFile(wayBundleFile, '', err => {
+const wayBundleFile = path.join(wayProject, 'bundle.css');
+
+fs.access(wayBundleFile, err => {
+  if (err) {
+    console.log('файла bundle.css НЕТ...');
+    fs.writeFile(wayBundleFile, '', (err) => { if (err) throw err; });
+    appendInStyleFile();
+  } else {
+    console.log('файл bundle.css ЕСТЬ');
+    fs.truncate(wayBundleFile, err => {
       if (err) throw err;
-    }
-)}
-  //fs.truncate(wayBundleFile, err => {
-  //  if (err) throw err;
-  //})
-})
-fs.readdir(wayStyles, (err, files) => {
-  if (err) throw err;
-  files.forEach(file => {
-    if (path.extname(file).includes('.css')) {
-      // console.log(file)
-      const input = fs.readFile(path.join(wayStyles, file), 'utf-8', (err, data) => {
-        if (err) throw err;
-        fs.appendFile(wayBundleFile, data, err => {
+      console.log('Файл bundle очищен');
+      appendInStyleFile();
+    });
+  }
+});
+
+function appendInStyleFile() {
+  fs.readdir(wayStyles, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      if (path.extname(file).includes('.css')) {
+        console.log(`added ${file}`);
+        fs.readFile(path.join(wayStyles, file), 'utf-8', (err, data) => {
           if (err) throw err;
-        })})
-    }
-  })
-})
+          fs.appendFile(wayBundleFile, data, err => {
+            if (err) throw err;
+          });});
+      }
+    });
+  });
+}
+
